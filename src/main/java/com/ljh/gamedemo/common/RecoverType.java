04e5fcd165067853afd2a6c50082566f903f49ce
linjiahong <heiiku@qq.com>
2019-09-12 18:32:24 +0800

    9.12 整理运行任务类的代码模块，重新编写对应的功能

---
+++ src/main/java/com/ljh/gamedemo/common/RecoverType.java
@@ -0,0 +1,26 @@
+package com.ljh.gamedemo.common;
+
+/**
+ * 恢复的基础信息
+ *
+ * @Author: Heiku
+ * @Date: 2019/9/12
+ */
+
+public class RecoverType {
+
+    /**
+     * 玩家的最大蓝量值
+     */
+    public static final int MAX_MP = 500;
+
+    /**
+     * 玩家自动恢复生命的速度
+     */
+    public static final int DEFAULT_HP_SEC = 2;
+
+    /**
+     * 玩家自动恢复蓝量的速度
+     */
+    public static final int DEFAULT_MP_SEC = 1;
+}
